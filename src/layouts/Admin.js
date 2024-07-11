35aaaae7e084c20611d337a63d7a64e3cbbf8d53
Chandru0811 <chandru08112000@gmail.com>
2024-07-11 14:42:00 +0530

    Global color added

--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -3,11 +3,20 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "../pages/admin/Dashboard";
 import HeaderAdmin from "../components/admin/common/HeaderAdmin";
 import FooterAdmin from "../components/admin/common/FooterAdmin";
+import { Toaster } from "react-hot-toast";
 
 function Admin({ handleLogout }) {
   return (
     <>
       <BrowserRouter>
+        <Toaster
+          toastOptions={{
+            style: {
+              background: "rgb(51 65 85)",
+              color: "#fff",
+            },
+          }}
+        />
         <HeaderAdmin />
         <Routes>
           <Route path="/" element={<Dashboard handleLogout={handleLogout} />} />
--- src/layouts/Client.js
+++ src/layouts/Client.js
@@ -3,11 +3,20 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/client/Home";
 import Header from "../components/client/common/Header";
 import Footer from "../components/client/common/Footer";
+import { Toaster } from "react-hot-toast";
 
 function Client({ handleLogin }) {
   return (
     <>
       <BrowserRouter>
+        <Toaster
+          toastOptions={{
+            style: {
+              background: "rgb(51 65 85)",
+              color: "#fff",
+            },
+          }}
+        />
         <Header />
         <Routes>
           <Route path="/" element={<Home handleLogin={handleLogin} />} />
