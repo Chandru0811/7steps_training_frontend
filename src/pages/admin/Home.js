d65e0731d2d96ccb41180ade39d3a381395a7f15
Sridaran04dec <sri04122001@gmail.com>
2024-07-12 16:38:42 +0530

    updated

--- src/pages/admin/Home.js
+++ src/pages/admin/Home.js
@@ -1,25 +1,27 @@
-import React from 'react';
+import React from "react";
+import AdminHome from "./AdminHome/AdminHome";
 
 function Home() {
-    return (
-      <section className="bg-white py-2">
-        <div className="container-fluid">
-          <div className="row">
-            <div className="d-flex justify-content-between align-items-center">
-              <h5 className="fw-bold">Home</h5>
-              <div>
-                {/* <button type="button" className="btn btn-light btn-sm mx-3">
+  return (
+    <section className="bg-white">
+      <div className="container-fluid py-2">
+        <div className="row">
+          <div className="d-flex justify-content-between align-items-center">
+            <h5 className="fw-bold">Home</h5>
+            <div>
+              {/* <button type="button" className="btn btn-light btn-sm mx-3">
                   Back
                 </button> */}
-                <button type="button" className="btn btn-button btn-sm">
-                  Publish
-                </button>
-              </div>
+              <button type="button" className="btn btn-button btn-sm">
+                Publish
+              </button>
             </div>
           </div>
         </div>
-      </section>
-    );
+      </div>
+      <AdminHome />
+    </section>
+  );
 }
 
 export default Home;
